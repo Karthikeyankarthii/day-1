fd6b1de82444aeee0b7de9992ce1177774984e15
Karthikeyankarthii <karthi989406@gmail.com>
2024-08-23 21:40:56 +0530

    karthi

---
+++ karthi.js
@@ -0,0 +1,8 @@
+function speak() {
+    const input = document.getElementById('input')
+    const text = input.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+
+
+}
